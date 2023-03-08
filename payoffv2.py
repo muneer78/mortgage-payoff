b1263f9950516416c2f516f0256d17b6c7af8cfa
muneer78 <muneer@duck.com>
2023-03-07 22:25:49 -0600

    Updated code to group by final payment amount.

---
+++ payoffv2.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+import numpy_financial as npf
+from datetime import date, datetime
+from numpy import random
+
+interest=0.04125
+years=2
+payments_year=12
+mortgage=44903.72
+start_date=(date(2023, 3, 1))
+
+initial_pmt=965.99
+initial_ipmt = -1 * npf.ipmt(interest/payments_year, 1, years*payments_year, mortgage)
+initial_ppmt = -1 * npf.ppmt(interest/payments_year, 1, years*payments_year, mortgage)
+
+# Create date range in pandas dataframe
+rng = pd.date_range(start_date, periods=years * payments_year, freq='MS')
+
+
+# label the date column
+rng.name="Payment Date"
+
+# create dataframe
+df=pd.DataFrame(
+    index=rng,
+    columns= ['Org Total Payment',
+              'Total Payment',
+              'Interest',
+              'Principal',
+              'Additional Payment',
+              'Org Ending Balance',
+              'Ending Balance'], dtype='float')
+# set index as payment period
+df.reset_index(inplace=True)
+df.index += 1
+df.index.name="Period"
+
+# Create values for the first period
+period=1
+additional_pmt = np.random.randint(2100, 4500, size=len(df))
+
+# for each element in the row set the value
+initial_row_dict = {
+    'Org Total Payment':initial_pmt,
+    'Total Payment': initial_pmt + (additional_pmt),
+    'Interest': initial_ipmt,
+    'Principal':initial_ppmt,
+    'Additional Payment': additional_pmt,
+    'Org Ending Balance': mortgage - initial_ppmt,
+    'Ending Balance': mortgage - initial_ppmt - (additional_pmt)
+}
+
+# set values
+columns = list(initial_row_dict.keys())
+period_values = list(initial_row_dict.values())
+df.at[period, columns]= period_values
+
+# add additional rows
+for period in range(2, len(df) + 1):
+    #get prior period values
+    previous_total_payment = df.loc[period - 1, 'Total Payment']
+    previous_principal = df.loc[period - 1, 'Principal']
+    previous_org_ending_balance = df.loc[period - 1, 'Org Ending Balance']
+    previous_ending_balance = df.loc[period - 1, 'Ending Balance']
+    #get end balance
+    period_interest = previous_org_ending_balance * interest / payments_year
+    period_principal = initial_pmt - period_interest
+    additional_pmt = np.random.randint(2100, 4500)
+    org_ending_balance = previous_org_ending_balance - period_principal
+    ending_balance = previous_ending_balance - period_principal - additional_pmt
+
+
+    row_dict = {'Org Total Payment':initial_pmt,
+                'Total Payment': initial_pmt + (additional_pmt),
+                'Interest': period_interest,
+                'Principal': period_principal,
+                'Additional Payment': additional_pmt,
+                'Org Ending Balance': org_ending_balance,
+                'Ending Balance': ending_balance}
+    columns = list(row_dict.keys())
+    period_values = list(row_dict.values())
+    df.at[period, columns]= period_values
+
+df_mask=df['Ending Balance']>=17500
+filtered_df = df[df_mask].round(2)
+
+filtered_df.to_csv('payoff.csv')
